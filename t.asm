0d7d90e71523712a2c7e001d38676eab5a3f3477
Evan Blouet <152662177+eblouet42@users.noreply.github.com>
2025-06-08 01:18:32 +0200

    Clarity of git repo
    bye bye

--- t.asm
+++
@@ -1,7 +0,0 @@
-    mov rax, [x]
-    add rax, 8
-    push rax
-    mov rax, [y]
-    sub rax, 3
-    pop [y]
-    add [y], rax
--- truc.asm
+++
@@ -1,2 +0,0 @@
-Optimized assembly written to /home/kakouzz/Desktop/lark_bark/Compilation-Group2/simple_optimized.asm
-/home/kakouzz/Desktop/lark_bark/Compilation-Group2/simple.asm
